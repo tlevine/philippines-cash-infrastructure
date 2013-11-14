f5c2087235b6ac922a54b62388d3333b38568df9
Thomas Levine <occurrence@thomaslevine.com>
2013-11-14 08:03:13 -0800

    stubs

--- post.py
+++ post.py
@@ -17,7 +17,17 @@ def download_municipalities():
     html = lxml.html.fromstring(r.text)
     return map(unicode, html.xpath('//select/descendant::option/@value'))
 
-print(download_municipalities())
+def download_results(municipality):
+    '''
+    >>> download_municipalities(u'Agusan Del Norte').split('\n')[0]
+    <h2>Result(s)</h2>
+    '''
+
+def parse_results(html_result_string):
+    '''
+    HTML string -> [dict]
+    '''
+    return [{}]
 
 if __name__ == '__main__':
     import doctest
