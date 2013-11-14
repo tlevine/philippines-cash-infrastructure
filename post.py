d7fca30c8bc8de63e7e765a815d1e4759b74bfaf
Thomas Levine <occurrence@thomaslevine.com>
2013-11-14 09:22:24 -0800

    oeu

--- post.py
+++ post.py
@@ -19,18 +19,49 @@ def download_parse_provinces():
     html = lxml.html.fromstring(r.text)
     return map(unicode, html.xpath('//select/descendant::option/@value'))
 
-def download_results(province):
+def download_results(province, session = None):
     '''
+    Mimic this Javascript
+
+        $(".province").change(function(){
+          var id=$(this).val();
+          var dataString = 'id='+ id;
+          $.ajax ({
+            type: "POST",
+            url: "postoffice-municipality.php",
+            data: dataString,
+            cache: false,
+            success: function(html) { $("#result").html(html); }
+          });
+        });
+
+    Note the lack of url-encoding.
+
     >>> len(lxml.html.parse(download_results(u'Agusan Del Norte')).xpath('//tr'))
     7
     '''
+    if session == None:
+        session = requests.session()
+        session.get('https://www.phlpost.gov.ph/post-office-location.php')
 
-    r = requests.post(
+    headers = {
+        u'Accept-Encoding': u'gzip,deflate,sdch',
+        u'Accept-Language': u'en-US,en;q=0.8,fr;q=0.6,sv;q=0.4,zh;q=0.2,zh-CN;q=0.2,zh-TW;q=0.2',
+        u'Host': u'www.phlpost.gov.ph',
+        u'Origin': u'https://www.phlpost.gov.ph',
+        u'Referer': u'https://www.phlpost.gov.ph/post-office-location.php',
+        u'User-Agent': u'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+        u'X-Requested-With': u'XMLHttpRequest',
+    }
+
+    r = session.post(
         'https://www.phlpost.gov.ph/postoffice-province.php',
-        data = {u'id': province},
-        headers = {u'X-Requested-With': u'XMLHttpRequest'})
+        data = u'id=' + province, headers = headers)
 
-    return r.text
+    print r.request.headers
+    print r.request.body
+
+    return r #.text
 
 def parse_results(province, html_result_string):
     '''
@@ -86,5 +117,6 @@ def main():
     return big_df
 
 if __name__ == '__main__':
+    res = download_results(u'Agusan Del Norte')
     # df = main()
-    test()
+    # test()
